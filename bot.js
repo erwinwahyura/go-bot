ad4451f4cdfb980b0ac40d9ed5d946d5ce8819fc
erwinwahyura <erwinwahyuramadhan@gmail.com>
2022-08-17 23:53:42 +0700

    remove comment

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { askAI } = require("./ai.js");
 
-const token = process.env.TOKEN; //Token that you saved in step 5 of this tutorial
+const token = process.env.TOKEN;
 const {Client, GatewayIntentBits} = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
@@ -12,10 +12,10 @@ client.on("ready", () =>{
 
 client.on("messageCreate", async (message) => {
     if (message.content !== "" && message.content.substring(0,22) === "<@1009478937405034517>") {
-        const prompt = message.content.substring(23); //remove the exclamation mark from the message
-        const answer = await askAI(prompt); //prompt GPT-3
+        const prompt = message.content.substring(23); 
+        const answer = await askAI(prompt);
         await message.channel.sendTyping()
-        await message.channel.send(answer); //reply to Discord with answer from GPT-3 
+        await message.channel.send(answer); 
     }
 });
 client.login(token);
